23e873f96917dab3d16e268bf0c25a3245b05da8
victor.martinelli <victor.martinelli@ifood.com.br>
2021-04-19 16:51:19 -0300

    fix: add new code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import * as http from 'http';
 import { Pool } from 'pg';
+import express from 'express';
 
-const pool = new Pool();
+const app = express();
 
-http
-  .createServer(async function (req, res) {
-    const data = await pool.query('SELECT NOW()');
-    res.write(data.rows);
-    res.end();
-  })
-  .listen(9090);
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT as any,
+});
 
-console.log('Server started on port 9090, Happy Hacking!');
+app.get('/', async (req, res) => {
+  const data = await pool.query('SELECT NOW()');
+  console.log(data.rows[0]);
+  res.send('Chamada a cobrar, para aceita-la diga seu nome e a cidade de onde está falando');
+});
+
+app.listen(9090, () => {
+  console.log('Server started on port 9090, Happy Hacking!');
+});
